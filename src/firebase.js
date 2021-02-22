5f6316526a6640a0ad69fd81ba33f2e18a704d8c
Michael Canon <69116412+mjcanon4@users.noreply.github.com>
2021-02-22 00:12:31 -0500

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAE4HBMBpPvOQ6A7RjkfmxfP1arQXWpIpI",
+  authDomain: "slack-app-5280e.firebaseapp.com",
+  projectId: "slack-app-5280e",
+  storageBucket: "slack-app-5280e.appspot.com",
+  messagingSenderId: "1080625870354",
+  appId: "1:1080625870354:web:a7bd04e7c27e50720ccacb",
+};
