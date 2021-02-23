a1fcdc9380866d02c18512f7ab5df56715c46d96
Michael Canon <69116412+mjcanon4@users.noreply.github.com>
2021-02-22 23:28:44 -0500

    header  done

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import { Avatar } from "@material-ui/core";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import SearchIcon from "@material-ui/icons/Search";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
 
 function Header() {
   return (
@@ -15,16 +17,62 @@ function Header() {
       </HeaderLeft>
 
       {/* Header Middle */}
+      <HeaderSearch>
+        <SearchIcon />
+        <input placeholder="Search Slack" />
+      </HeaderSearch>
 
       {/* Header Right */}
+      <HeaderRight>
+        <HelpOutlineIcon />
+      </HeaderRight>
     </HeaderContainer>
   );
 }
 
 export default Header;
 
+const HeaderRight = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: flex-end;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 20px;
+  }
+`;
+
+const HeaderSearch = styled.div`
+  flex: 0.4;
+  opacity: 1;
+  border-radius: 6px;
+  background-color: #421f44;
+  text-align: center;
+  display: flex;
+  padding: 0 50px;
+  color: gray;
+  border: 1px gray solid;
+
+  > input {
+    background-color: transparent;
+    border: none;
+    text-align: none;
+    min-width: 30vw;
+    outline: 0;
+    color: white;
+  }
+`;
+
 const HeaderContainer = styled.div`
   display: flex;
+  position: fixed;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 0;
+  background-color: var(--slack-color);
+  color: white;
 `;
 
 const HeaderLeft = styled.div`
