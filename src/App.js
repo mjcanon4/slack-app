6110fa0b6e5951402108272b87b17e23804f4208
Michael Canon <69116412+mjcanon4@users.noreply.github.com>
2021-02-22 23:10:55 -0500

    introduced styled components and material ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>Let's Build Slack</h1>
+      <Router>
+        <>
+          <Switch>
+            <Route path="/" exact>
+              <Header />
+            </Route>
+          </Switch>
+        </>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,47 @@
+import React from "react";
+import styled from "styled-components";
+import { Avatar } from "@material-ui/core";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+
+function Header() {
+  return (
+    <HeaderContainer>
+      {/* Header Left */}
+      <HeaderLeft>
+        <HeaderAvatar
+        //TODO: ADD ONCLICK
+        />
+        <AccessTimeIcon />
+      </HeaderLeft>
+
+      {/* Header Middle */}
+
+      {/* Header Right */}
+    </HeaderContainer>
+  );
+}
+
+export default Header;
+
+const HeaderContainer = styled.div`
+  display: flex;
+`;
+
+const HeaderLeft = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 30px;
+  }
+`;
+
+const HeaderAvatar = styled(Avatar)`
+  cursor: pointer;
+  :hover {
+    opacity: 0.8;
+  }
+`;
