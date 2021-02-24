d73bbcbb5a19b74512055de1d33d5a52b1bdd42a
Michael Canon <69116412+mjcanon4@users.noreply.github.com>
2021-02-24 00:13:38 -0500

    handling firestore

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import styled from "styled-components";
 import { db } from "../firebase";
+import { enterRoom } from "../features/appSlice";
 
-function SidebarOption({ Icon, title, addChannelOption }) {
+function SidebarOption({ Icon, title, addChannelOption, id }) {
+  const dispatch = useDispatch();
+
   const addChannel = () => {
     const channelName = prompt("Please enter the channel name");
 
@@ -13,7 +17,16 @@ function SidebarOption({ Icon, title, addChannelOption }) {
     }
   };
 
-  const selectChannel = () => {};
+  const selectChannel = () => {
+    if (id) {
+      dispatch(
+        enterRoom({
+          roomId: id,
+        })
+      );
+    }
+  };
+
   return (
     <SidebarOptionContainer
       onClick={addChannelOption ? addChannel : selectChannel}
@@ -53,4 +66,7 @@ const SidebarOptionContainer = styled.div`
   }
 `;
 
-const SidebarOptionChannel = styled.div``;
+const SidebarOptionChannel = styled.h3`
+  padding: 10px 0;
+  font-weight: 300;
+`;
